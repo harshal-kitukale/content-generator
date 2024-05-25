c422223cf40c4abf3dcefe6401051bd6c17c615c
harshal-kitukale <harshalkitukale005@gmail.com>
2024-05-25 16:11:34 +0530

    changed shayari app ri to content-generator

---
+++ Backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+app.use(express.json());
+app.use(cors());
+
+const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY);
+
+app.post("/content", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    console.log(prompt);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const result = await model.generateContent(
+      [prompt],
+      SafetySetting = [
+        {
+          category: "HARM_CATEGORY_HARASSMENT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_HATE_SPEECH",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+      ]
+    );
+    console.log("jkk", result.response.candidates[0].finishReason);
+    if (result.response.candidates[0].finishReason == "SAFETY") {
+      return res.status(400).json({
+        error:
+          "content is blocked due safety, be specific in your command or please try another command",
+        content: [],
+      });
+    }
+
+    res.json({ content: result.response.candidates[0].content.parts[0].text, error: "" });
+    // res.json({ content: result.response.text() });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
